ca2f4d93c13f62c2b50b617ee8825fc1aab5f02a
oxcm07 <oxcm0727@gmail.com>
2026-07-15 11:34:09 +0900

    04

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ package com
 
 fun main() {
     println("== 명언 앱 ==")
+    var count = 0
 
     while (true) {
         print("명령) ")
@@ -11,7 +12,8 @@ fun main() {
             val sentence = readLine()
             print("작가 : ")
             val author = readLine()
-            print("1번 명언이 등록되었습니다.")
+            count++
+            println("${count}번 명언이 등록되었습니다.")
         }
         if (input == "종료") break
     }
