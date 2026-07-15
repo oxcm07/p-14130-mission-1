11386075816f14e6297cdde585e2339d2fe27d4a
oxcm07 <oxcm0727@gmail.com>
2026-07-15 12:00:27 +0900

    06

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,8 @@ package com
 
 fun main() {
     println("== 명언 앱 ==")
-    var sentences = mutableListOf<String>()
-    var authors = mutableListOf<String>()
+    val wiseSayings = mutableListOf<WiseSaying>()
+    var count = 0
 
     while (true) {
         print("명령) ")
@@ -11,19 +11,28 @@ fun main() {
         if (input == "등록") {
             print("명언 : ")
             val sentence = readlnOrNull()
-            sentence?.let { sentences.add(it) }
             print("작가 : ")
             val author = readlnOrNull()
-            author?.let { authors.add(it) }
-            println("${sentences.size}번 명언이 등록되었습니다.")
+            ++count
+            wiseSayings.add(WiseSaying(count, sentence!!, author!!))
+            println("${count}번 명언이 등록되었습니다.")
         }
         if (input == "목록") {
             println("번호 / 작가 / 명언")
             println("----------------------")
-            sentences.indices.reversed().forEach { index ->
-                println("${index + 1} / ${authors[index]} / ${sentences[index]}")
+            wiseSayings.asReversed().forEach { saying ->
+                println("${saying.id} / ${saying.author} / ${saying.sentence}")
+            }
+        }
+        input?.let {
+            if (it.startsWith("삭제?id=")) {
+                val id = input.substringAfter("id=").toInt()
+                wiseSayings.remove(wiseSayings.find { saying -> saying.id == id })
+                println("${id}번 명언이 삭제되었습니다.")
             }
         }
         if (input == "종료") break
     }
 }
+
+data class WiseSaying(val id: Int, val sentence: String, val author: String)
