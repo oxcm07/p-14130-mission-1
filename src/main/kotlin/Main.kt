5e96969e012101c29011ccb4c1ea1a16f3080cb3
oxcm07 <oxcm0727@gmail.com>
2026-07-15 12:20:39 +0900

    08

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,6 +8,7 @@ fun main() {
     while (true) {
         print("명령) ")
         val input = readlnOrNull()
+
         if (input == "등록") {
             print("명언 : ")
             val sentence = readlnOrNull()
@@ -17,6 +18,7 @@ fun main() {
             wiseSayings.add(WiseSaying(count, sentence!!, author!!))
             println("${count}번 명언이 등록되었습니다.")
         }
+
         if (input == "목록") {
             println("번호 / 작가 / 명언")
             println("----------------------")
@@ -24,6 +26,7 @@ fun main() {
                 println("${saying.id} / ${saying.author} / ${saying.sentence}")
             }
         }
+
         input?.let {
             if (it.startsWith("삭제?id=")) {
                 val id = it.substringAfter("id=").toIntOrNull()
@@ -35,7 +38,25 @@ fun main() {
                     println("${id}번 명언은 존재하지 않습니다.")
                 }
             }
+
+            if (it.startsWith("수정?id=")) {
+                val id = it.substringAfter("id=").toIntOrNull()
+                val found = wiseSayings.find { saying -> saying.id == id }
+                if(found != null) {
+                    println("명언(기존) : ${found.sentence}")
+                    print("명언 : ")
+                    val newSentence = readlnOrNull()
+                    println("작가(기존) : ${found.author}")
+                    print("작가 : ")
+                    val newAuthor = readlnOrNull()
+                    wiseSayings.remove(found)
+                    id?.let { it -> wiseSayings.add(WiseSaying(it, newSentence!!, newAuthor!!)) }
+                } else {
+                    println("${id}번 명언은 존재하지 않습니다.")
+                }
+            }
         }
+
         if (input == "종료") break
     }
 }
