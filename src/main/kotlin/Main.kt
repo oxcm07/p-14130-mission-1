2d48f15d61a72749cda597c9cb4fe7f90e36ac23
oxcm07 <oxcm0727@gmail.com>
2026-07-15 11:49:35 +0900

    05

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,27 @@ package com
 
 fun main() {
     println("== 명언 앱 ==")
-    var count = 0
+    var sentences = mutableListOf<String>()
+    var authors = mutableListOf<String>()
 
     while (true) {
         print("명령) ")
-        val input = readLine()
-        if (input == "등록"){
+        val input = readlnOrNull()
+        if (input == "등록") {
             print("명언 : ")
-            val sentence = readLine()
+            val sentence = readlnOrNull()
+            sentence?.let { sentences.add(it) }
             print("작가 : ")
-            val author = readLine()
-            count++
-            println("${count}번 명언이 등록되었습니다.")
+            val author = readlnOrNull()
+            author?.let { authors.add(it) }
+            println("${sentences.size}번 명언이 등록되었습니다.")
+        }
+        if (input == "목록") {
+            println("번호 / 작가 / 명언")
+            println("----------------------")
+            sentences.indices.reversed().forEach { index ->
+                println("${index + 1} / ${authors[index]} / ${sentences[index]}")
+            }
         }
         if (input == "종료") break
     }
