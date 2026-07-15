c65a85a08a3cc075ec829f4f987586fb97d31abf
oxcm07 <oxcm0727@gmail.com>
2026-07-15 11:31:33 +0900

    02

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,14 @@ fun main() {
     println("== 명언 앱 ==")
 
     while (true) {
-        println("명령) ")
+        print("명령) ")
         val input = readLine()
+        if (input == "등록"){
+            print("명언 : ")
+            val sentence = readLine()
+            print("작가 : ")
+            val author = readLine()
+        }
         if (input == "종료") break
     }
 }
